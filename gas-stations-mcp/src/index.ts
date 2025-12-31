b786cb6dbc9a493770c88b3b6493d6523f751e54
Stefan <stefan@lenovo14.local>
2025-12-31 21:46:56 +0100

    add description

--- gas-stations-mcp/src/index.ts
+++ gas-stations-mcp/src/index.ts
@@ -27,7 +27,7 @@ server.registerTool(
             lng: z.number().describe("Longitude as a number e.g. 7."),
             rad: z.number().min(0).max(25).describe("Radius/Max distance as number to be used for search in km."),
             limit: z.number().min(0).optional().default(10).describe("Maximum number of stations to return. Default is 10. Set to 0 for unlimited."),
-            type: z.optional(z.enum(['e5', 'e10', 'diesel', 'all'])),
+            type: z.optional(z.enum(['e5', 'e10', 'diesel', 'all'])).describe("Type of fuel to filter by. Options are 'e5', 'e10', 'diesel', or 'all'. Default is 'all'."),
             sort: z.optional(z.enum(['price', 'dist'])).describe("Sort by price or distance. Only applicable when type is not 'all'. If type is set to 'all', sort must be ommitted or set to 'dist'."),
             withDetails: z.boolean().optional().default(false).describe("If true, includes more details about each station in the response. Default is false. Fetching details using this query is not recommended."),
         },
